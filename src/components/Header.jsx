cea6b486d6b1d26ca862fe3d99cd8b67895a4ebc
Krzysztof <zajackrzysztof00@gmail.com>
2025-06-11 17:15:44 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,16 +4,16 @@ import { NavLink } from 'react-router-dom'
 export default function Header() {
   return (
     <nav>
-      <NavLink to="/Portfolio/" end className={({ isActive }) => isActive ? 'active' : ''}>
+      <NavLink to="/" end className={({ isActive }) => isActive ? 'active' : ''}>
         Main
       </NavLink>
-      <NavLink to="/Portfolio/education" className={({ isActive }) => isActive ? 'active' : ''}>
+      <NavLink to="/education" className={({ isActive }) => isActive ? 'active' : ''}>
         Education
       </NavLink>
-      <NavLink to="/Portfolio/experience" className={({ isActive }) => isActive ? 'active' : ''}>
+      <NavLink to="/experience" className={({ isActive }) => isActive ? 'active' : ''}>
         Experience
       </NavLink>
-      <NavLink to="/Portfolio/contact" className={({ isActive }) => isActive ? 'active' : ''}>
+      <NavLink to="/contact" className={({ isActive }) => isActive ? 'active' : ''}>
         Contact
       </NavLink>
     </nav>
